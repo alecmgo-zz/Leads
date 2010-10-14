1ec8e9397d0586f17f0c047fc3effccf97073fd6
Robby Walker <robby.walker@gmail.com>
2010-10-14 15:36:45 -0700

    Now able to (fake) get, save, etc

--- static/leads.js
+++ static/leads.js
@@ -45,24 +45,83 @@ function addField(container, item, fieldName, displayName, fieldType) {
 	var elem = $('<div></div>')
 		.addClass('field')
 		.append($('<span></span>').text(displayName + ':').addClass("field"))
-		.append($('<span></span>').attr('id', fieldName).text(item[fieldName] || "").addClass("value"));
-	container.append(elem);	
+		.append($('<input type="text"></input>')
+			.attr('id', fieldName)
+			.attr('name', fieldName)
+			.val(item[fieldName] || "")
+			.addClass("value")
+			.focus(function() {
+				$(this).addClass("active");
+			})
+			.blur(function() {
+				$(this).removeClass("active");
+			})
+		);
+	container.append(elem);
 }
 
 function displayItem(item) {
 	var container = $('#workspaceBody');
 	container.empty();
+	container.append($('<input type="hidden" name="id"></input>').val(item['id'] || ''));
 	addField(container, item, 'title', 'Title', 'text');
 	for (var i = 0, len = fields.length; i < len; i++) {
 		var field = fields[i];
 		addField(container, item, field['name'], field['displayName'], field['type'])
 	}
 	addField(container, item, 'active', 'Active', 'boolean');
-	container.append($('<div id="content"></div>').text(item['content']));
+	var content = $('<div id="content" class="autogrow"></div>').append(
+		$('<textarea></textarea>').attr('name', 'content').text(item['content']));
+	container.append(content);
+	container.append($('<div id="save"></div>').append($('<button type="submit">Save</button>')).click(save));
+	autoGrow(content);
+}
+
+function autoGrow(element) {
+	var top = element.offset().top;
+	var bottom = element.next().offset().top || element.parent().height();
+	
+	var height = bottom - top;
+	element.css('height', height + 'px');
+	element.css('height', height + height - element.outerHeight());		
+}
+
+function updateAutoGrow() {
+	$('.autogrow').each(function() {
+		autoGrow($(this));
+	})
+}
+
+function getFieldValue(result, name, dataType) {
+	var fieldElem = $('[name=' + name + ']');
+	result[name] = fieldElem.val() || fieldElem.text();
+}
+
+function save() {
+	var result = {};
+	getFieldValue(result, 'id');
+	getFieldValue(result, 'title');
+	getFieldValue(result, 'active');
+	for (var i = 0, len = fields.length; i < len; i++) {
+		var field = fields[i];
+		getFieldValue(result, field['name'], field['type']);
+	}
+	getFieldValue(result, 'content');
+	$('#save button').text('Saving...')
+	$.ajax({
+		url: "api/saveItem",
+		data: JSON.stringify(result),
+		success: function(newId) {
+			$('[name=id]').val(newId['id']);
+			$('#save button').text('Save');
+		}
+	})
 }
 
 $().ready(function() {
 	$('#loading').show();
+	
+	$(window).resize(updateAutoGrow);
 	
 	$.ajaxSetup({
 		cache: false,
